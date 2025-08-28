63e04f167f43fef779ba6fbed0d301b2edc8b009
Teknovashop <teknovashop25@gmail.com>
2025-08-28 13:10:29 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,2 +1,11 @@
 import type { Config } from 'tailwindcss'
-export default {content:['./**/*.{js,ts,jsx,tsx}'],theme:{extend:{}},plugins:[],} satisfies Config
+
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config
